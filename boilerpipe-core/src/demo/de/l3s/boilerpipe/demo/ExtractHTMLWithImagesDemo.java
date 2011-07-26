6748a66ee0d53d3b91a40581b1d96a3cb458e17f
Greg Bayer <greg@gbayer.com>
2011-07-25 18:21:02 -0700

    Added example for extracting html with images.

---
+++ boilerpipe-core/src/demo/de/l3s/boilerpipe/demo/ExtractHTMLWithImagesDemo.java
@@ -0,0 +1,51 @@
+package de.l3s.boilerpipe.demo;
+
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.l3s.boilerpipe.BoilerpipeExtractor;
+import de.l3s.boilerpipe.extractors.CommonExtractors;
+import de.l3s.boilerpipe.sax.HTMLHighlighter;
+
+/**
+ * Demonstrates how to use Boilerpipe to get the main content, highlighted as HTML.
+ * 
+ * @author Christian Kohlschütter
+ * @see Oneliner if you only need the plain text.
+ */
+public class ExtractHTMLWithImagesDemo {
+	public static void main(String[] args) throws Exception {
+		URL url = new URL(
+//				"http://research.microsoft.com/en-us/um/people/ryenw/hcir2010/challenge.html"
+//				"http://boilerpipe-web.appspot.com/"
+//				"http://mashable.com/2011/07/19/bill-gates-reinvent-toilet/"
+				"http://techcrunch.com/2011/07/21/do-authors-dream-of-electric-book-signings-kindlegraph-hopes-so/"
+		        );
+		
+		// choose from a set of useful BoilerpipeExtractors...
+		final BoilerpipeExtractor extractor = CommonExtractors.ARTICLE_EXTRACTOR;
+//		final BoilerpipeExtractor extractor = CommonExtractors.DEFAULT_EXTRACTOR;
+//		final BoilerpipeExtractor extractor = CommonExtractors.CANOLA_EXTRACTOR;
+//		final BoilerpipeExtractor extractor = CommonExtractors.LARGEST_CONTENT_EXTRACTOR;
+
+		// choose the operation mode (i.e., highlighting or extraction)
+//		final HTMLHighlighter hh = HTMLHighlighter.newHighlightingInstance();
+		final boolean includeImages = true;
+		final HTMLHighlighter hh = HTMLHighlighter.newExtractingInstance(includeImages);
+		
+		PrintWriter out = new PrintWriter("/tmp/highlighted.html", "UTF-8");
+		out.println("<base href=\"" + url + "\" >");
+		out.println("<meta http-equiv=\"Content-Type\" content=\"text-html; charset=utf-8\" />");
+		
+		String extractedHtml = hh.process(url, extractor);
+		out.println(extractedHtml);
+
+		out.close();
+		
+		System.out.println("Now open file:///tmp/highlighted.html in your web browser");
+	}
+}
